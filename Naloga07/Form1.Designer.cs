6278b3410279b6b2b846b5ecb68046fd8bdbd196
DeliciousShoesHD <bizjaktomaz72@gmail.com>
2019-01-06 15:11:15 +0100

    Dodana Naloga07

---
+++ Naloga07/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Naloga07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pristevajOption = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.odstevajOption = new System.Windows.Forms.RadioButton();
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pristevajOption
+            // 
+            this.pristevajOption.AutoSize = true;
+            this.pristevajOption.Checked = true;
+            this.pristevajOption.Location = new System.Drawing.Point(6, 19);
+            this.pristevajOption.Name = "pristevajOption";
+            this.pristevajOption.Size = new System.Drawing.Size(65, 17);
+            this.pristevajOption.TabIndex = 1;
+            this.pristevajOption.TabStop = true;
+            this.pristevajOption.Text = "Prištevaj";
+            this.pristevajOption.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.odstevajOption);
+            this.groupBox1.Controls.Add(this.pristevajOption);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(159, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // odstevajOption
+            // 
+            this.odstevajOption.AutoSize = true;
+            this.odstevajOption.Location = new System.Drawing.Point(6, 42);
+            this.odstevajOption.Name = "odstevajOption";
+            this.odstevajOption.Size = new System.Drawing.Size(67, 17);
+            this.odstevajOption.TabIndex = 2;
+            this.odstevajOption.Text = "Odštevaj";
+            this.odstevajOption.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(197, 10);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 145);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 178);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Start";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton pristevajOption;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton odstevajOption;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Naloga07/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naloga07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                int stevilo;
+                if (!int.TryParse(textBox1.Text, out stevilo))
+                {
+                    MessageBox.Show("To število ni mogoče pretvoriti.");
+                    textBox1.Text = "";
+                    startButton.Enabled = false;
+                }
+                else
+                {
+                    if (stevilo >= 1 && stevilo <= 100)
+                    {
+                        startButton.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Število ni v dovoljenem obsegu!");
+                        textBox1.Text = "";
+                        startButton.Enabled = false;
+                    }
+                }
+            }else startButton.Enabled = false;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = int.Parse(textBox1.Text);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pristevajOption.Checked)
+            {
+                if (progressBar1.Value != progressBar1.Maximum)
+                {
+                    if (progressBar1.Maximum > (progressBar1.Value + progressBar1.Step))
+                    {
+                        progressBar1.Value += progressBar1.Step;
+                    }
+                    else {
+                        progressBar1.Value = progressBar1.Maximum;
+                        startButton.Enabled = false;
+                        timer1.Stop();
+                    }
+                }else
+                {
+                    startButton.Enabled = false;
+                    timer1.Stop();
+                }
+            }
+            else if (odstevajOption.Checked)
+            {
+                if (progressBar1.Value != progressBar1.Minimum)
+                {
+                    if (progressBar1.Minimum < (progressBar1.Value - progressBar1.Step))
+                    {
+                        progressBar1.Value -= progressBar1.Step;
+                    }
+                    else
+                    {
+                        progressBar1.Value = progressBar1.Minimum;
+                        startButton.Enabled = false;
+                        timer1.Stop();
+                    }
+                }else
+                {
+                    startButton.Enabled = false;
+                    timer1.Stop();
+                }
+            }
+        }
+    }
+}
